9bcf7a7e66121641739142fbaf393b394e4b4ee0
MaximoSavall <102547236+Maxpower56@users.noreply.github.com>
2025-11-18 19:38:34 -0300

    uniendo logica

--- trabajo_final/interfaz_grafica/datos/constantes.py
+++ trabajo_final/interfaz_grafica/datos/constantes.py
@@ -7,6 +7,6 @@ TITLE = config['default']['ventana']['titulo']
 COLOR_FONDO = config['default']['colores']['fondo']
 COLOR_TEXTO_OSCURO = config['default']['colores']['texto_oscuro']
 COLOR_TEXTO_CLARO = config['default']['colores']['texto_claro']
-COLOR_TEXTO_SECUNDARIO = config['default']['colores']['secundario']
+COLOR_SECUNDARIO = config['default']['colores']['secundario']
 VOLUMEN = config['default']['audio']['vol']
 
--- trabajo_final/interfaz_grafica/main.py
+++ trabajo_final/interfaz_grafica/main.py
@@ -2,7 +2,7 @@ import pygame
 import datos.constantes as con
 from render.render_pantalla import pantalla_principal, pantalla_opciones
 from audio.musica import reproducir_musica, MUSICA_PRINCIPAL
-from eventos import nuestros_eventos
+from eventos.nuestros_eventos import gestionar_eventos
 
 
 pygame.init()
@@ -14,6 +14,7 @@ FPS = 60
 pantalla_actual = "menu"
 musica_actual = None
 
+
 botones = None
 
 ejecutando = True
@@ -22,6 +23,8 @@ while ejecutando:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             ejecutando = False
+        else:
+            pantalla_actual = gestionar_eventos(evento, pantalla_actual, botones)
 
     # Lógica de juego
     if pantalla_actual == "menu":
@@ -29,8 +32,9 @@ while ejecutando:
             reproducir_musica(MUSICA_PRINCIPAL)
             musica_actual = "menu"
         botones = pantalla_principal(pantalla)
+
     elif pantalla_actual == "jugar":
-        botones = None
+        pass
     elif pantalla_actual == "opciones":
         botones = pantalla_opciones(pantalla)
     elif pantalla_actual == "creditos":
@@ -38,7 +42,7 @@ while ejecutando:
     elif pantalla_actual == "estadisticas":
         botones = None
     elif pantalla_actual == "salir":
-        pass
+        ejecutando = False
     
     # Actualización de pantalla
     pygame.display.flip()
